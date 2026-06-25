defe3588ef54663053ad291ac75ad8b3a1480ffb
yaseieneko <yaseieneko@gmail.com>
2026-06-25 15:52:57 +0900

    Use built Tailwind CSS for production

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        shippori: ["Shippori Mincho", "serif"],
+        cormorant: ["Cormorant Garamond", "serif"],
+        cinzel: ["Cinzel", "serif"],
+        fell: ["IM Fell English SC", "serif"],
+        yuji: ["Yuji Syuku", "Shippori Mincho", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
